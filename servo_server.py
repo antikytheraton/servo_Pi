847e99b36a17260a2c291add9ab824da1d419350
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-10-17 20:32:21 -0500

    returns

--- servo_server.py
+++ servo_server.py
@@ -16,10 +16,25 @@ def hello(request):
 
 @app.route('/openGas', methods=['POST'])
 def openGas():
+    '''
+    Endpoint apertura de paso de gas
+    '''
     p.ChangeDutyCycle(4.5)
     print('Abriendo paso de gas')
+    return 'Arbiendo paso de gas'
 
 @app.route('/closeGas', methods=['POST'])
 def closeGas():
+    '''
+    Endpoint cerrado de gas
+    '''
     p.ChangeDutyCycle(10.5)
     print('Cerrando paso de gas')
+    return 'Cerrando paso de gas'
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=80,
+        debug=True
+    )
